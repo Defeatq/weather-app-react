f9abb23d272c7e14449cfc9703057282743b869c
Artem <awrer1234eads@gmail.com>
2022-06-26 23:22:34 +0300

    add rtk store

--- src/js/store.js
+++ src/js/store.js
@@ -2,12 +2,18 @@ import thunk from "redux-thunk";
 import { createStore } from "redux";
 import { applyMiddleware } from "redux";
 import weatherInit from "./reducers";
+import { configureStore } from "@reduxjs/toolkit";
 
-const store = createStore(
-  weatherInit,
-  applyMiddleware(
-    thunk
-  )
-);
+const store = configureStore({
+  reducer: weatherInit,
+  middleware: [ thunk ],
+});
+
+// const store = createStore(
+//   weatherInit,
+//   applyMiddleware(
+//     thunk
+//   )
+// );
 
 export default store
